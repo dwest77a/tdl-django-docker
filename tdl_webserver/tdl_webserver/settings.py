f67fbda30475ec90649c32e7377adae828a97460
dwest77a <daniel.westwood@stfc.ac.uk>
2022-10-07 13:23:54 +0100

    First successful docker image build completed - basic webpage appearance

--- tdl_webserver/tdl_webserver/settings.py
+++ tdl_webserver/tdl_webserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n#ti5lliy!rjm$=_ew-shfduuc^rsu=dr6*e0dubn76c$tk+qy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
